cfc4e962731004d9812e7c444af3cbbc3c2633da
Arnauldfohom <arnauldfohom1@gmail.com>
2026-08-14 15:22:06 +0100

    python basics day1

---
+++ Basics/day1.py
@@ -0,0 +1,25 @@
+#print() function
+
+# print("My first python program!")
+# print(25)
+# print(3.14)
+
+
+#Variables
+
+name = "Arnauld"
+age= 26
+height= 1.85
+
+# print(name,age,height)
+print("My name is",name, "I'm", age , 'and Im' , height, 'tall')
+
+is_student = True
+has_bachelor = True
+is_dating = False
+
+
+print(type(name))
+print(type(is_student))
+print(type(height))
+print(type(age))
